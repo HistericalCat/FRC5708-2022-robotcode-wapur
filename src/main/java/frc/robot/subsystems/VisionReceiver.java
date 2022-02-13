c46342eb357057d5855b5e4d05db61ace1af895c
sebwyn <gogleboywerser@gmail.com>
2022-02-13 15:39:14 -0500

    updated visionreceiver
    Co-authored-by: jonahlee1 <jonahlee1@users.noreply.github.com>
    Co-authored-by: HistericalCat <HistericalCat@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/VisionReceiver.java
+++ src/main/java/frc/robot/subsystems/VisionReceiver.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems;
 
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.SocketException;
 import java.net.UnknownHostException;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -9,12 +10,19 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class VisionReceiver extends SubsystemBase{
     private DatagramSocket socket;
     private InetAddress ip;
+    private int port = 5808;
 
     public VisionReceiver(){
         setupSocket();
     }
 
     private void setupSocket(){
+        try {
+            socket = new DatagramSocket();
+        } catch(SocketException ex){
+            ex.printStackTrace();
+        }
+
         try {
             ip = InetAddress.getByName("raspberrypi.local");
         System.out.println(ip);
@@ -23,4 +31,10 @@ public class VisionReceiver extends SubsystemBase{
         }
         
     }
+    
+    @Override
+    public void periodic(){
+        char buf = [66537];
+    }
+    
 }
