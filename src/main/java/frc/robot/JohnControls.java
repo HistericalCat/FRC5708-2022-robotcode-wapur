35858702a84f0f0551d82a90170a65bb7a338738
Team 5708 Laptop <github@uni-nets.com>
2022-03-13 14:37:21 -0400

    Makes new controls for John

--- src/main/java/frc/robot/JohnControls.java
+++ src/main/java/frc/robot/JohnControls.java
@@ -5,19 +5,33 @@ import frc.robot.ControlScheme;
 public class JohnControls extends ControlScheme {
     @Override
     public double getWinch1Power(){
-        double power = Control.getXboxCtrl().getRightTriggerAxis() - Control.getXboxCtrl().getLeftTriggerAxis();
-        if(power > 0.05 || power < -0.05){
+
+        double power = Control.getXboxCtrl().getRightY();
+        if(power>0.05 || power< -0.05){
             return power;
         }
         return 0;
+    
     }
 
     @Override
     public double getWinch2Power(){
-        double power = Control.getXboxCtrl().getLeftTriggerAxis() - Control.getXboxCtrl().getRightTriggerAxis();
-        if(power > 0.05 || power<-0.05){
+        double power = Control.getXboxCtrl().getLeftY();
+        if(power>0.05 || power<-0.05){
             return power;
         }
+        return 0; 
+    }
+    @Override
+    public double getActPower(){
+        double power = Control.getXboxCtrl().getRightTriggerAxis() - Control.getXboxCtrl().getLeftTriggerAxis();
+        if(power > 0.05 || power < -0.05){
+            return power;
+        }
+        
         return 0;
     }
+
+
+
 }
